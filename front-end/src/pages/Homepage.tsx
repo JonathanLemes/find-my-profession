3576d9b8ca77b3185ab9e43203ea34ac98aad52f
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-11-12 19:14:22 -0300

    build(front-end): homepage getting services
    starting the integration between the front-end and back-end, getting the services from the API and
    showing them at homepage

--- front-end/src/pages/Homepage.tsx
+++ front-end/src/pages/Homepage.tsx
@@ -1,50 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/pages/homepage.css';
 
 import Navbar from '../components/Navbar';
+import api from '../services/api';
+
+interface Service {
+    name: string,
+    description: string,
+    url: string
+}
 
 export default function Homepage() {
-  return (
-    <div className="App">
-        <Navbar />
-        <main role="main">
-            <div className="jumbotron">
+    const [services, setServices] = useState<Service[]>([]);
+
+    useEffect(() => {
+        api.get('/services').then((response) => {
+            setServices(response.data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <Navbar />
+            <main role="main">
+                <div className="jumbotron">
+                    <div className="container">
+                        <h1 className="display-3">Hello, world!</h1>
+                        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                        <p><a className="btn btn-primary btn-lg" href="/" role="button">Learn more &raquo;</a></p>
+                    </div>
+                </div>
+
                 <div className="container">
-                    <h1 className="display-3">Hello, world!</h1>
-                    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                    <p><a className="btn btn-primary btn-lg" href="/" role="button">Learn more &raquo;</a></p>
-                </div>
-            </div>
+                    <div className="row">
+                        {services.map((service, index) => {
+                            return (
+                                <div className="col-md-4" key={index}>
+                                    <h2>{service.name}</h2>
+                                    <p>{service.description}</p>
+                                    <p><a className="btn btn-secondary" href="/" role="button">View details &raquo;</a></p>
+                                </div>
+                            )
+                        })}
+                    </div>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a className="btn btn-secondary" href="/" role="button">View details &raquo;</a></p>
-                    </div>
-                    <div className="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a className="btn btn-secondary" href="/" role="button">View details &raquo;</a></p>
-                    </div>
-                    <div className="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                        <p><a className="btn btn-secondary" href="/" role="button">View details &raquo;</a></p>
-                    </div>
+                    <hr />
+
                 </div>
 
-                <hr />
+            </main>
 
-            </div>
-
-        </main>
-
-        <footer className="container">
-        <p>&copy; Company 2017-2020</p>
-        </footer>
-    </div>
-  );
+            <footer className="container">
+            <p>&copy; Company 2017-2020</p>
+            </footer>
+        </div>
+    );
 }
