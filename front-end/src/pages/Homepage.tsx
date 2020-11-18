803ea536cde3d79f4f2e6e72746939a2e15f50ec
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-11-18 11:52:46 -0300

    style(homepage and pricing page): loading animation added
    the animation react-loading was added, before de back-end data is loaded

--- front-end/src/pages/Homepage.tsx
+++ front-end/src/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import ReactLoading from 'react-loading';
 
 import '../styles/pages/homepage.css';
 
@@ -20,6 +21,37 @@ export default function Homepage() {
             setServices(response.data);
         });
     }, []);
+
+    if (services.length === 0) {
+        return (
+            <div className="App">
+                <Navbar />
+                <main role="main">
+                    <div className="jumbotron">
+                        <div className="container">
+                            <h1 className="display-3">Hello, world!</h1>
+                            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                            <p><a className="btn btn-primary btn-lg" href="/" role="button">Learn more &raquo;</a></p>
+                        </div>
+                    </div>
+
+                    <div className="container">
+                        <div className="row loading">
+                            <ReactLoading type={"spin"} color={'#6c757d'}/>
+                        </div>
+
+                        <hr />
+
+                    </div>
+
+                </main>
+
+                <footer className="container">
+                <p>&copy; Company 2017-2020</p>
+                </footer>
+            </div>
+        )
+    }
 
     return (
         <div className="App">
--- front-end/src/pages/Pricing.tsx
+++ front-end/src/pages/Pricing.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
 
 import '../styles/pages/pricing.css';
 
@@ -36,6 +37,61 @@ export default function Pricing() {
             setService(response.data);
         });
     }, [params.url]);
+
+    if (tiers.length === 0 || !service) {
+        return (
+            <div className="Pricing">
+                <Navbar />
+
+                <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center loading">
+                    <ReactLoading type={"spin"} color={'#6c757d'}/>
+                </div>
+
+                <div className="container">
+                    <div className="card-deck mb-3 text-center">
+                        <ReactLoading type={"spin"} color={'#6c757d'}/>
+                    </div>
+                </div>
+
+                <footer className="pt-4 my-md-5 pt-md-5 border-top">
+                    <div className="row">
+                        <div className="col-12 col-md">
+                            <small className="d-block mb-3 text-muted">&copy; 2017-2020</small>
+                        </div>
+                        <div className="col-6 col-md">
+                            <h5>Features</h5>
+                            <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="/">Cool stuff</a></li>
+                            <li><a className="text-muted" href="/">Random feature</a></li>
+                            <li><a className="text-muted" href="/">Team feature</a></li>
+                            <li><a className="text-muted" href="/">Stuff for developers</a></li>
+                            <li><a className="text-muted" href="/">Another one</a></li>
+                            <li><a className="text-muted" href="/">Last time</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-md">
+                            <h5>Resources</h5>
+                            <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="/">Resource</a></li>
+                            <li><a className="text-muted" href="/">Resource name</a></li>
+                            <li><a className="text-muted" href="/">Another resource</a></li>
+                            <li><a className="text-muted" href="/">Final resource</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-md">
+                            <h5>About</h5>
+                            <ul className="list-unstyled text-small">
+                            <li><a className="text-muted" href="/">Team</a></li>
+                            <li><a className="text-muted" href="/">Locations</a></li>
+                            <li><a className="text-muted" href="/">Privacy</a></li>
+                            <li><a className="text-muted" href="/">Terms</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        )
+    }
 
     return (
         <div className="Pricing">
