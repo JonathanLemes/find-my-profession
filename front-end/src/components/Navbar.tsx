85d0dcc3bbba70985c20261da0d97cc318102cf8
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-11-18 16:35:24 -0300

    docs(api): the api documentation now opens in a new tab

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="https://app.swaggerhub.com/apis-docs/JonathanLemes/Find-my-profession/1.0.0">API Documentation</a>
+                        <a className="nav-link" rel="noreferrer" target="_blank" href="https://app.swaggerhub.com/apis-docs/JonathanLemes/Find-my-profession/">API Documentation</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
