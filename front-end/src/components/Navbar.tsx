e327b2424053b90f3b3822e0ebcfdb7c695b17b7
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-11-18 16:28:34 -0300

    docs(api): api documentation link
    the navbar now contains a direct link to the api documentation page

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="https://github.com/JonathanLemes/find-my-profession/tree/master/docs/api.html">API Documentation</a>
+                        <a className="nav-link" href="https://app.swaggerhub.com/apis-docs/JonathanLemes/Find-my-profession/1.0.0">API Documentation</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
